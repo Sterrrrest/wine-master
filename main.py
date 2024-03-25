70092222605a1b5a6a64463b527030530e9108af
Sterrrrest <sterrrrest@gmail.com>
2024-03-25 22:45:39 +0300

    Удалил неиспользуемый код

--- main.py
+++ main.py
@@ -14,18 +14,17 @@ if __name__ == '__main__':
     parser.add_argument('-f', '--file_name', help='Имя файла', default=environment('FILE_NAME'))
     parser.add_argument('-l', '--file_list', help='Название страницы', default=environment('SHEET_NAME'))
     args = parser.parse_args()
-
+    foundation_date = 1920
     excel_data_df2 = pandas.read_excel(args.file_name, sheet_name=args.file_list, na_values=['N/A','NA'], keep_default_na=False)
 
     products_from_file = excel_data_df2.to_dict('records')
     products = collections.defaultdict(list)
-    prices = collections.defaultdict(list)
 
     for product_from_file in products_from_file:
         products[product_from_file['Категория']].append(product_from_file)
 
 
-    age = (datetime.datetime.today().year - 1920)
+    age = datetime.datetime.today().year - foundation_date
     last_digit = (str(age)[-1])
     if (last_digit == '2' or last_digit == '3' or last_digit == '4') and (str(age)[-2] != '1'):
         ages = f"{age} года"
@@ -45,7 +44,7 @@ if __name__ == '__main__':
         products_from_file=products_from_file, products=products, ages=ages
     )
 
-    with open('template.html', 'w', encoding="utf8") as file:
+    with open('index.html', 'w', encoding="utf8") as file:
         file.write(rendered_page)
 
     server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
